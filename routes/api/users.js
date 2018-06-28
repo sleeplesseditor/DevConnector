630f8cdf257df451568823097a544c29c8a2aaf1
sleeplesseditor <josh@bryson.me.uk>
2018-06-28 15:37:15 +0100

    Login route setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,31 @@ router.post('/register', (req, res) => {
         })
 });
 
+//ROUTE: GET request to API/users/login
+//DESC: Login user/Returning JWT Token
+//ACCESS: Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email})
+        .then(user => {
+            //Check for user
+            if(!user) {
+                return res.status(404).json({email: 'User not found'});
+            }
+
+            //Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                })
+        });
+});
+
 module.exports = router;
