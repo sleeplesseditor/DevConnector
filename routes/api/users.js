b8d72a8095f4f70ff6fd8fb00cc177f4bd738717
sleeplesseditor <josh@bryson.me.uk>
2018-06-29 16:13:40 +0100

    Validation handlers updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //Load User model
 const User = require('../../models/User');
@@ -65,6 +66,14 @@ router.post('/register', (req, res) => {
 //DESC: Login user/Returning JWT Token
 //ACCESS: Public
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    //Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -73,7 +82,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             //Check for user
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             //Check Password
@@ -95,7 +105,8 @@ router.post('/login', (req, res) => {
                                 });
                         });
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 })
         });
