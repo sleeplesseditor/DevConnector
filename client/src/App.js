6e213b44a63e8b9b95ff3ec307bbe8f3411f63d7
sleeplesseditor <josh@bryson.me.uk>
2018-07-05 13:47:33 +0100

    Logout system setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import store from './store';
 import './App.css';
 
@@ -21,6 +21,17 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //ToDo: clear current profile
+    
+    //Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -20,6 +20,12 @@ import { registerUser } from '../../actions/authActions';
      this.onSubmit = this.onSubmit.bind(this);
    }
 
+  componentDidMount() {
+    if(this.props.auth.isAuthenticated) {
+        this.props.history.push('/dashboard');
+    }
+  }
+
    componentWillReceiveProps(nextProps) {
      if(nextProps.errors) {
       this.setState({ errors: nextProps.errors });
