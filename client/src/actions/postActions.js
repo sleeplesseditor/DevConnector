78a627d079ec1156973b33a214560575196dd6ca
sleeplesseditor <josh@bryson.me.uk>
2018-07-10 11:28:49 +0100

    PostFeed and PostItem component setup

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {
     ADD_POST,
-    GET_ERRORS
+    GET_ERRORS,
+    GET_POSTS,
+    POST_LOADING
 } from './types';
 
 //Add Post
@@ -20,4 +22,30 @@ export const addPost = postData => dispatch => {
                 payload: err.response.data
             })
         );
-}
+};
+
+//Get Posts
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading());
+    axios
+        .get('/api/posts')
+        .then(res => 
+            dispatch({
+                type: GET_POSTS,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_POSTS,
+                payload: null
+            })
+        );
+};
+
+//Set loading state
+export const setPostLoading = () => {
+    return {
+        type: POST_LOADING
+    }
+};
